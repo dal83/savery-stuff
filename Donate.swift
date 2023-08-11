0eeabbda968db4d49fdd7524ccd025ba1b4e3d93
dal83 <126291855+dal83@users.noreply.github.com>
2023-08-11 12:35:27 -0400

    Add files via upload

---
+++ Donate.swift
@@ -0,0 +1,19 @@
+//
+//  Donate.swift
+//  Savery
+//
+//  Created by Deborah Lilly on 7/21/20.
+//  Copyright © 2020 SaveryApp. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+//donate stuff
+struct DonateView: View {
+    var body: some View {
+        NavigationView {
+            Text("placeholder")
+        }
+    }
+}
---
+++ InTheNews.swift
@@ -0,0 +1,19 @@
+//
+//  InTheNews.swift
+//  Savery
+//
+//  Created by Deborah Lilly on 7/21/20.
+//  Copyright © 2020 SaveryApp. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+// news api stuff
+struct NewsView: View {
+    var body: some View {
+        NavigationView {
+            Text("placeholder")
+        }
+    }
+}
