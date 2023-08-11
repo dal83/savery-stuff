0ea28a3fadd01616115047e787f6de2b3f82e20f
dal83 <126291855+dal83@users.noreply.github.com>
2023-08-11 12:48:41 -0400

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  Savery
+//
+//  Created by Deborah Lilly on 7/21/20.
+//  Copyright © 2020 SaveryApp. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        //congifure AdMob
+        GADMobileAds.configure(withApplicationID: "ca-app-pub-6930120295531865~9511554242")
+        
+        //set up user's unique ID
+        /*Auth.auth().signInAnonymously() { (authResult, error) in
+            guard let user = authResult?.user else { return }
+            let isAnonymous = user.isAnonymous  // true
+            let uid = user.uid
+        }*/
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
